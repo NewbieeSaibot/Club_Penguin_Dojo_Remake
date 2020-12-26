022f9fdb193de63bb0485c10b6cfd6658c306bac
Tobias Rossi Müller <48110428+NewbieeSaibot@users.noreply.github.com>
2020-12-26 20:48:52 -0300

    Add files via upload
     Add some new features to almost every class.

--- engine/card.py
+++ engine/card.py
@@ -1,8 +1,11 @@
 from engine.render_able_object import RenderAbleObject
+from engine.interact_able_object import InteractAbleObject
+from threading import Thread
+import pygame
 
 
-class Card(RenderAbleObject):
-    def __init__(self, id_card, name, element, force, image_path, x=0, y=0, width=100, height=150):
+class Card(RenderAbleObject, InteractAbleObject):
+    def __init__(self, id_card, name, element, force, image_path, x=0, y=0, width=100, height=150, cool_down=5):
         super().__init__(x, y, width, height)
         # Variables
         self.id_card = id_card
@@ -11,3 +14,22 @@ class Card(RenderAbleObject):
         self.force = force
         self.image_path = image_path
         self.image = None
+        self.player = None
+        self.interact_able = False
+        self.cool_down = cool_down
+        self.cool_down_current = True
+
+    def interact(self, keys, mouse):
+        if self.interact_able:
+            if mouse.get_pressed()[0] and self.is_inside(mouse.get_pos()[0], mouse.get_pos()[1]):
+                if self.player is not None:
+                    self.cool_down_current = False
+                    self.player.selected_card = self
+                    timer = Thread(target=self.wait)
+                    timer.start()
+
+    def wait(self):
+        clock = pygame.time.Clock()
+        for i in range(self.cool_down):
+            clock.tick(20)
+        self.cool_down_current = True
--- engine/player.py
+++ engine/player.py
@@ -1,16 +1,38 @@
 from engine.render_able_object import RenderAbleObject
 from engine.interact_able_object import InteractAbleObject
+from threading import Lock
+import random
+
+mutex = Lock()
 
 
 class Player(RenderAbleObject, InteractAbleObject):
     def __init__(self, x=50, y=50, width=20, height=20):
         super().__init__(x, y, width, height)
-
         # Variables
         self.deck = []
         self.hand = []
+        self.selected_card = None
+        self.ai = False
 
     def interact(self, keys, mouse):
-        pass
-        # Maybe select the card if the click is going to select something or
-        # move the character, whatever
+        for i in range(len(self.hand)):
+            self.hand[i].interact(keys, mouse)
+
+    def draw(self, window):
+        mutex.acquire()
+        for i in range(len(self.hand)):
+            self.hand[i].draw(window)
+        mutex.release()
+
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+
+    def take_cards(self, number):
+        if len(self.deck) <= number:
+            return False
+
+        for i in range(number):
+            self.hand.append(self.deck.pop())
+
+        return True
